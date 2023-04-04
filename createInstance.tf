201d8e858925651e504cc721125f7df7ad6c183f
YiYangHsu <yiyanghsu@gmail.com>
2023-04-04 13:34:13 -0400

    image_id_output

--- createInstance.tf
+++ createInstance.tf
@@ -1,11 +1,28 @@
 provider "aws" {
     access_key = "AKIATCP27AVA5AKQRR2M"
     secret_key = "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"
-    region  = "us-east-2"
+    region  = "us-east-1"
 }
 
-resource "aws_instance" "MyFirstInstance" {
-    ami = "	ami-0a561b65214a47cac"
-    instance_type = "t2.micro"
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "owner-alias"
+        values = ["amazon"]
+    }
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm*"]
+    }
 }
+
+output "ec2_instance_id" {
+    value = aws_ami.latest-amazon-linux-image
+}
+# resource "aws_instance" "MyFirstInstance" {
+#     ami = "	ami-0a561b65214a47cac"
+#     instance_type = "t2.micro"
+# }
  
