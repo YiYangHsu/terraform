2aa3f18186df50a384383d97cbc1ec74ea90f774
YiYangHsu <yiyanghsu@gmail.com>
2023-04-05 14:57:32 -0400

    resource edit

--- createInstance.tf
+++ createInstance.tf
@@ -43,26 +43,25 @@ resource "aws_instance" "MyFirstInstance" {
     tags = {
         Name = "custom_instance"
     }
-
+    provisioner "file" {
+        source = "installNginx.sh"
+        destination = "/tmp/installNginx.sh"
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/installNginx.sh",
+            "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",  # Remove the spurious CR characters.
+            "sudo /tmp/installNginx.sh",
+        ]
+    }    
+    connection {
+        host = coalesce(self.public_ip,self.private_ip)
+        type = "ssh"
+        user = var.INSTANCE_USERNAME
+        private_key = file(var.PATH_TO_PRIVATE_KEY)
+    }
 }
 
-provisioner "file" {
-    source = "installNginx.sh"
-    destination = "/tmp/installNginx.sh"
-}
 
-provisioner "remote-exec" {
-    inline = [
-        "chmod =x /tmp/installNginx.sh",
-        "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",
-        "sudo /tmp/installNginx.sh"
-    ]
-    
-}
 
-connection {
-    host = coalesce(self.public_ip,self.private_ip)
-    type = "ssh"
-    user = var.INSTANCE_USERNAME
-    private_key = file(var.PATH_TO_PRIVATE_KEY)
-}
+
