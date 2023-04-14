94236cac91dcb6bd36d83b06fb3c2f1103158dca
YiYangHsu <yiyanghsu@gmail.com>
2023-04-14 13:39:39 -0400

    DataSource in Terraform 1

--- createInstance.tf
+++ createInstance.tf
@@ -1,10 +1,4 @@
 
-resource "aws_key_pair" "levelup_key" {
-    key_name = "levelup_key"
-    public_key = file(var.PATH_TO_PUBLIC_KEY)
-}
-
-
 data "aws_ami" "latest-amazon-linux-image" {
     most_recent = true
 
@@ -38,28 +32,12 @@ resource "aws_instance" "MyFirstInstance" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     # ami = lookup(var.AMIS, var.AWS_REGION)
     instance_type = "t2.micro"
-    key_name = aws_key_pair.levelup_key.key_name
 
     tags = {
         Name = "custom_instance"
     }
-    provisioner "file" {
-        source = "installNginx.sh"
-        destination = "/tmp/installNginx.sh"
-    }
-    provisioner "remote-exec" {
-        inline = [
-            "chmod +x /tmp/installNginx.sh",
-            "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",  # Remove the spurious CR characters.
-            "sudo /tmp/installNginx.sh",
-        ]
-    }    
-    connection {
-        host = coalesce(self.public_ip,self.private_ip)
-        type = "ssh"
-        user = var.INSTANCE_USERNAME
-        private_key = file(var.PATH_TO_PRIVATE_KEY)
-    }
+
+
 }
 
 
